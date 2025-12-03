ddcf63007644d83cc69b1ddb83925b397e0e79ff
zonor121 <kopinator9000@gmail.com>
2025-12-03 22:29:08 +0300

    Перемещён тип графа в верхнюю панлеьку

--- test2.py
+++ test2.py
@@ -895,7 +895,11 @@ class ModernGraphVisualizer(QMainWindow):
         top_layout.addWidget(header_widget)
         
         top_layout.addStretch(1)
-        
+
+        # Группа типа графа
+        graph_type_group = self.create_graph_type_group()
+        top_layout.addWidget(graph_type_group)
+
         algo_group = QGroupBox("Выбор алгоритма")
         algo_group.setStyleSheet(f"""
             QGroupBox {{
@@ -1063,10 +1067,7 @@ class ModernGraphVisualizer(QMainWindow):
         right_widget = QWidget()
         right_layout = QVBoxLayout(right_widget)
         right_layout.setSpacing(10)
-        
-        graph_type_group = self.create_graph_type_group()
-        right_layout.addWidget(graph_type_group)
-        
+
         # Группа выбора узлов
         node_group = QGroupBox("Выбор узлов")
         node_group.setStyleSheet(self.get_groupbox_style())
